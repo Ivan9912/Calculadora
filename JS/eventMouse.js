6bd7f3965b41e23b4dcc7c748d6ce556e914ecb4
Ivan Maidana <ivan.maidana9912@gmail.com>
2022-07-16 20:39:10 -0300

    arreglo de los botones part 2

--- JS/eventMouse.js
+++ JS/eventMouse.js
@@ -1,5 +1,5 @@
-const calculateButton = document.getElementById ("buttonCalculator").value;
-calculateButton.addEventListener("mouseover", () => {calculateButton.setAttribute ("style", "background-color:#001851; color:white; border: 3px solid #ffffff;");});
-calculateButton.addEventListener("mouseout", () => {calculateButton.setAttribute ("style", "background-color:white; color:#001851; border: 3px solid #001851;");});
+const calculateButton = document.getElementById ("buttonCalculator");
+calculateButton.addEventListener("mouseover", () => {calculateButton.setAttribute ("style", "background-color:#001851; color:white; border: 2px solid #ffffff;");});
+calculateButton.addEventListener("mouseout", () => {calculateButton.setAttribute ("style", "background-color:white; color:#001851; border: 2px solid #001851;");});
 
 
