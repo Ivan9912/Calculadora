823f18b8dca1a9d2ea032928314f4ecbbd1903ea
Ivan Maidana <ivan.maidana9912@gmail.com>
2022-07-16 20:25:29 -0300

    retoques visuales

--- JS/eventButtonFooter.js
+++ JS/eventButtonFooter.js
@@ -1,4 +1,5 @@
-const buttonAux = document.getElementById ("boton__enviar");
-buttonAux.addEventListener ("mouseover", () => {buttonAux.setAttribute ("style", "background-color:#24303c; color:white;");});
-buttonAux.addEventListener ("mouseout", () => {buttonAux.setAttribute ("style", "background-color:white; color:#24303c");});
+const buttonAux;
+buttonAux = document.getElementById ("boton__enviar");
+buttonAux.addEventListener ("mouseover", () => {buttonAux.setAttribute ("style", "background-color:#001851; color:white; border: 3px solid #ffffff;");});
+buttonAux.addEventListener ("mouseout", () => {buttonAux.setAttribute ("style", "background-color:white; color:#001851; border: 3px solid #001851;");});
 buttonAux.addEventListener ("click", () => {alert ("Este botón aún no contiene una funcionalidad. \nPor favor, vuelve a intentar otro día.");});
--- JS/eventMouse.js
+++ JS/eventMouse.js
@@ -1,5 +1,6 @@
-const calculateButton = document.getElementById ("buttonCalculator");
-calculateButton.addEventListener("mouseover", () => {calculateButton.setAttribute ("style", "background-color:#24303c; color:white;");});
-calculateButton.addEventListener("mouseout", () => {calculateButton.setAttribute ("style", "background-color:white; color:#24303c");});
+const calculateButton;
+calculateButton = document.getElementById ("buttonCalculator").value;
+calculateButton.addEventListener("mouseover", () => {calculateButton.setAttribute ("style", "background-color:#001851; color:white; border: 3px solid #ffffff;");});
+calculateButton.addEventListener("mouseout", () => {calculateButton.setAttribute ("style", "background-color:white; color:#001851; border: 3px solid #001851;");});
 
 
