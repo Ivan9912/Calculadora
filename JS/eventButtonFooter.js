705bf2f66c28e735f1d593762a17eb747fc08a63
Ivan Maidana <ivan.maidana9912@gmail.com>
2022-07-16 14:14:35 -0300

    reponsive con CSS

--- JS/eventButtonFooter.js
+++ JS/eventButtonFooter.js
@@ -1,3 +1,4 @@
 const buttonAux = document.getElementById ("boton__enviar");
-buttonAux.addEventListener("mouseover", () => {buttonAux.setAttribute ("style", "background-color:#24303c; color:white;");});
-buttonAux.addEventListener("mouseout", () => {buttonAux.setAttribute ("style", "background-color:white; color:#24303c");});
+buttonAux.addEventListener ("mouseover", () => {buttonAux.setAttribute ("style", "background-color:#24303c; color:white;");});
+buttonAux.addEventListener ("mouseout", () => {buttonAux.setAttribute ("style", "background-color:white; color:#24303c");});
+buttonAux.addEventListener ("click", () => {alert ("Este botón aún no contiene una funcionalidad. \nPor favor, vuelve a intentar otro día.");});
