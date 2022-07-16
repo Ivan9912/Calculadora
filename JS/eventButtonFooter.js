a9b3fd82497c543bcb11e746d64a73254caa4af6
Ivan Maidana <ivan.maidana9912@gmail.com>
2022-07-16 20:33:40 -0300

    arreglo del los mouses enents

--- JS/eventButtonFooter.js
+++ JS/eventButtonFooter.js
@@ -1,5 +1,4 @@
-const buttonAux;
-buttonAux = document.getElementById ("boton__enviar");
+const buttonAux = document.getElementById ("boton__enviar");
 buttonAux.addEventListener ("mouseover", () => {buttonAux.setAttribute ("style", "background-color:#001851; color:white; border: 3px solid #ffffff;");});
 buttonAux.addEventListener ("mouseout", () => {buttonAux.setAttribute ("style", "background-color:white; color:#001851; border: 3px solid #001851;");});
 buttonAux.addEventListener ("click", () => {alert ("Este botón aún no contiene una funcionalidad. \nPor favor, vuelve a intentar otro día.");});
--- JS/eventMouse.js
+++ JS/eventMouse.js
@@ -1,5 +1,4 @@
-const calculateButton;
-calculateButton = document.getElementById ("buttonCalculator").value;
+const calculateButton = document.getElementById ("buttonCalculator").value;
 calculateButton.addEventListener("mouseover", () => {calculateButton.setAttribute ("style", "background-color:#001851; color:white; border: 3px solid #ffffff;");});
 calculateButton.addEventListener("mouseout", () => {calculateButton.setAttribute ("style", "background-color:white; color:#001851; border: 3px solid #001851;");});
 
